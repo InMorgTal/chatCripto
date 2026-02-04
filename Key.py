48635c1dda51ed0c427207d2748dec472c6e412e
NibHis <161223306+NibHis@users.noreply.github.com>
2026-02-04 14:39:46 +0100

    Update Key.py

--- Key.py
+++ Key.py
@@ -6,27 +6,24 @@ from Crypto.Protocol.KDF import PBKDF2
 from Crypto.Hash import SHA256
 import socket
 class Key:
-    contatore = 0
-    def __init__(self, key, tag, salt, password):
-        self.id = Key.contatore
+    def __init__(self, key, nonce = None):
         self.key = key
-        self.tag = tag
-        self.salt = salt
-        self.password = password
-        Key.contatore += 1
+        self.nonce = nonce
+        if(self.nonce = None)
+            self.nonce = get_random_bytes(16)
     def criptare(self, testo):
-        master_key = PBKDF2(self.password, self.salt, dkLen=32, count=200000)
-        cipher = AES.new(master_key, AES.MODE_GCM, nonce=self.id.to_bytes(16, byteorder='big'))
+        cipher = AES.new(self.key, AES.MODE_GCM, nonce=self.nonce)
         encrypted_text, tag = cipher.encrypt_and_digest(testo.encode())
-        return encrypted_text, tag
-    def decriptare(self, encrypted_text, tag):
-        master_key = PBKDF2(self.password, self.salt, dkLen=32, count=200000)
-        cipher = AES.new(master_key, AES.MODE_GCM, nonce=self.id.to_bytes(16, byteorder='big'))
+        nonce = self.nonce
+        self.nonce = get_random_bytes(16)
+        return encrypted_text, tag, nonce
+    def decriptare(self, encrypted_text, tag, nonce):
+        cipher = AES.new(self.key, AES.MODE_GCM, nonce=self.nonce)
         decrypted_text = cipher.decrypt_and_verify(encrypted_text, tag)
         return decrypted_text.decode()
     def to_string(self):
-        return f"{self.id},{self.key.hex()},{self.tag.hex()},{self.salt.hex()},{self.password}"
+        return f"{self.key.hex()}, {self.nonce.hex()}"
     def to_bytes(self):
-        return (f"{self.id},{self.key.hex()},{self.tag.hex()},{self.salt.hex()},{self.password}").encode()
+        return self.key + self.nonce
     def __str__(self):
         return f"Key(id={self.id}, key={self.key}, tag={self.tag}, salt={self.salt}, password={self.password})"
